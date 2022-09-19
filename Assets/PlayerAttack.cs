8b509c552e20c7f0348a71d1e3b8ec00f47900d2
fihra <fabrofabian@gmail.com>
2022-09-18 23:19:01 -1000

    added one strike attack animations and implemented attack in 4 directions mechanic

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private Animator animator;
+
+    private float attackTime = 0.25f;
+    private float attackCounter = 0.25f;
+    private bool isAttacking;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isAttacking)
+        {
+            attackCounter -= Time.deltaTime;
+            if(attackCounter <= 0)
+            {
+                animator.SetBool("isAttacking", false);
+                isAttacking = false;
+            }
+        }
+
         if(Input.GetKeyDown(KeyCode.Z))
         {
-            Debug.Log("Perform Attack");
+            attackCounter = attackTime;
+            Debug.Log("Do that one attack where you swing your stick or whatever");
+            animator.SetBool("isAttacking", true);
+            isAttacking = true;
         }
 
     }
